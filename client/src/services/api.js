9b249b213bea8dd66f5aa24724ea16cc14ac6f82
Pratham Chouhan <chouhanpratham422@gmail.com>
2026-09-23 22:13:21 +0530

    Update production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://notehub-extk.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
